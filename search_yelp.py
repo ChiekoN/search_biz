486eb8e75f58f134af34869bf747a68afe5fbaa4
Chieko Natori <chieko.natori@gmail.com>
2018-05-16 15:19:32 +0800

    Separate the process of searching next page to next_page_link().

--- search_yelp.py
+++ search_yelp.py
@@ -15,28 +15,30 @@ from requests_html import HTMLSession
 #  -- Message the business (optional)
 rest_list = dict()
 
+
+def next_page_link(next_element):
+    if next_element :
+        # next_link_element.absolute_links is set object,
+        # so return that one item as a text using min().
+        return min(next_element.absolute_links)
+    else:
+        return ''
+
 def crawl_main_list(top_url):
 
-    rest_selector = 'li.regular-search-result'
-    rest_name_selector = 'h3.search-result-title > span.indexed-biz-name > a.biz-name.js-analytics-click > span'
-    rest_genre_selector = 'div.price-category > span.category-str-list > a'
-    rest_secondary_selector = 'div.secondary-attributes'
-    rest_area_selector = 'span.neighborhood-str-list'
-    rest_address_selector = 'address'
-    rest_phone_selector = 'span.biz-phone'
 
     session = HTMLSession()
     req = session.get(top_url)
 
-    top_list = req.html.find(rest_selector)
+    top_list = req.html.find('li.regular-search-result')
 
     # Take information of restaurants from Main Page
     for a_rest in top_list:
-        rest_name = a_rest.find(rest_name_selector, first = True).text
-        rest_genre_list = [ rest_genre.text for rest_genre in a_rest.find(rest_genre_selector)]
-        rest_area = a_rest.find(rest_secondary_selector, first = True).find(rest_area_selector, first = True).text
-        rest_address = a_rest.find(rest_secondary_selector, first = True).find(rest_address_selector, first = True).text.replace('\n', ', ')
-        rest_phone = a_rest.find(rest_secondary_selector, first = True).find(rest_phone_selector, first = True).text
+        rest_name = a_rest.find('h3.search-result-title > span.indexed-biz-name > a.biz-name.js-analytics-click > span', first = True).text
+        rest_genre_list = [ rest_genre.text for rest_genre in a_rest.find('div.price-category > span.category-str-list > a')]
+        rest_area = a_rest.find('div.secondary-attributes', first = True).find('span.neighborhood-str-list', first = True).text
+        rest_address = a_rest.find('div.secondary-attributes', first = True).find('address', first = True).text.replace('\n', ', ')
+        rest_phone = a_rest.find('div.secondary-attributes', first = True).find('span.biz-phone', first = True).text
 
         # Set information to rest_list.
         rest_list[rest_name] =  { 'genre' : rest_genre_list,
@@ -45,15 +47,8 @@ def crawl_main_list(top_url):
                                   'phone' : rest_phone
                                 }
     # Pick up 'next page' link
-    next_selector = 'a.u-decoration-none.next.pagination-links_anchor'
-    next_link_element = req.html.find(next_selector, first = True)
-    if next_link_element :
-        # next_link_element.absolute_links is set object,
-        # so return that one item as a text using min().
-        return min(next_link_element.absolute_links)
-    else:
-        return ''
-
+    return next_page_link(req.html.find(
+                'a.u-decoration-none.next.pagination-links_anchor', first = True))
 
 def main():
 
